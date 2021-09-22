b534a1d0f946f077d59914f8f1b1ce7e163f8534
matry18 <matry18@student.sdu.dk>
2021-09-22 09:26:35 +0200

    Revert "Doing Kafka"
    This reverts commit df2547bca72e3b2d393fa201d366052eb1e28cfb.

--- demo/src/main/java/com/example/demo/controller.java
+++ demo/src/main/java/com/example/demo/controller.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "*")
