7c0c378570806e9161d287673f0c930bc57c69e2
matry18 <matry18@student.sdu.dk>
2021-10-12 12:43:37 +0200

    Adding Authentication service with DB in k8s

--- services/authentication/src/main/java/com/graduationProject/authentication/webservice/ResidentWebService.java
+++ services/authentication/src/main/java/com/graduationProject/authentication/webservice/ResidentWebService.java
@@ -1,23 +1,30 @@
 package com.graduationProject.authentication.webservice;
 
+import com.graduationProject.authentication.dto.ResidentDto;
 import com.graduationProject.authentication.producer.Producer;
+import com.graduationProject.authentication.service.ResidentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("*")
 @RestController
 public class ResidentWebService {
 
     private final Producer producer;
+    private final ResidentService residentService;
 
     @Autowired
-    public ResidentWebService(Producer producer) {
+    public ResidentWebService(ResidentService residentService, Producer producer) {
+        this.residentService = residentService;
         this.producer = producer;
     }
 
+    @PostMapping("authentication/test")
+    public ResidentDto testBackendConnectionToDB(@RequestBody ResidentDto residentDto){
+        residentService.addResident(residentDto);
+        System.out.println("Endpoint for authentication-test blev ramt");
+        return residentDto;
+    }
 
     @GetMapping("Authentication/test/{message}")
     public void authenticationKafkaPublish(@PathVariable String message) {
