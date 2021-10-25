c92e9e0c3b0af21d7a47f9a37c84a7e955fb9dcb
matry18 <matry18@student.sdu.dk>
2021-10-25 10:24:40 +0200

    Adding getAllEmployees test

--- services/bosted/src/test/java/com/graduationproject/bosted/EmployeeWebServiceTest.java
+++ services/bosted/src/test/java/com/graduationproject/bosted/EmployeeWebServiceTest.java
@@ -11,23 +11,25 @@ import com.graduationproject.bosted.webservice.EmployeeWebService;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.web.JsonPath;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.web.servlet.function.RequestPredicates.accept;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(EmployeeWebService.class)
@@ -40,10 +42,10 @@ public class EmployeeWebServiceTest {
     private EmployeeService employeeService;
 
     @MockBean
-    private EmployeeRepository employeeRepository;
+    private KafkaAPI kafkaAPI;
 
     @MockBean
-    private KafkaAPI kafkaAPI;
+    private EmployeeRepository employeeRepository;
 
     private Employee employee;
 
@@ -98,11 +100,22 @@ public class EmployeeWebServiceTest {
                 .andExpect(content().string(objectMapper.writeValueAsString(employeeDto)));
     }
 
-
     @Test
-    public void employeeWebService_getAllEmployees_shouldReturnFoo() throws  Exception {
-        
+    public void employeeWebService_getAllEmployees_shouldReturnList() throws  Exception {
+        employee = EmployeeFixture.createEmployee();
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(employee);
+        Mockito.when(employeeService.getAllEmployees()).thenReturn(employeeList);
+        EmployeeDto employeeDto = new EmployeeDto(employee);
+        ObjectMapper objectMapper = new ObjectMapper();
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/bosted/employees")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(employeeDto))
+                .contentType(MediaType.APPLICATION_JSON);
+        mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andReturn().equals(Arrays.asList(objectMapper.writeValueAsString(employeeDto)));
     }
-//    when(employeeService.getAllEmployees()).thenReturn(Collections.singletonList(employee));
 }
 
